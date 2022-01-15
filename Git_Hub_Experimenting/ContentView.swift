65771ad7898fc778f052aed54153afd540ac77fe
Arronpe <aeiesland75@hotmail.com>
2022-01-15 09:50:27 -0600

    Initial Commit

---
+++ Git_Hub_Experimenting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Git_Hub_Experimenting
+//
+//  Created by Arron Eiesland on 1/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Git_Hub_Experimenting/Git_Hub_ExperimentingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_Hub_ExperimentingApp.swift
+//  Git_Hub_Experimenting
+//
+//  Created by Arron Eiesland on 1/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Git_Hub_ExperimentingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
